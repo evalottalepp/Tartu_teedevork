50758a7aa63efdef2e29e4b50d6f88638733b84a
evalottalepp <evalepp@gmail.com>
2026-01-12 14:18:23 +0200

    Executable for running scripts

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+from src.api import fetch_all_features
+from src.process import process_roads
+
+def main():
+    gdf = fetch_all_features()
+    gdf = process_roads(gdf)
+
+    return gdf
+
+if __name__ == "__main__":
+    output = main()
+    print(len(output))
